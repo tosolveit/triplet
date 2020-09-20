0b334122222f68e30c4557f5e34154ab1508686c
macbook <macbook@84.local>
2020-09-20 18:49:25 +0200

    added metrics.txt to save precision

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,12 +1,10 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow import keras
 from annoy import AnnoyIndex
 from PIL import Image
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import array_to_img
-from helpers import extractzip
 import os
 import tqdm
 import random
@@ -16,7 +14,12 @@ from gempoolinglayer import GeMPoolingLayer
 
 
 # get configuration file
-cnf = Box.from_yaml(filename="config.yml", Loader=ruamel.yaml.Loader)
+p = os.path.abspath(os.path.join(__file__, "../.."))
+params_path = os.path.join(p, 'params.yaml')
+
+# read config based on params, params.yaml is at the project root
+cnf = Box.from_yaml(filename=params_path, Loader=ruamel.yaml.Loader)
+
 
 # load model
 model = tf.keras.models.load_model(
@@ -24,24 +27,21 @@ model = tf.keras.models.load_model(
      custom_objects={'GeMPoolingLayer': GeMPoolingLayer}
 )
 
-
-# write raw inputs to annoy index
-extractzip('pix512.zip')
-images_folder = 'pix512'
+images_folder = os.path.join(p, 'data/pix512')
 
 # initialize index object
 t = AnnoyIndex(cnf.model.embeddingsize, 'euclidean')
 
 # add predicted embedding values to index for the original images
 for i in os.listdir(images_folder):
-    index, extension  = i.split('.')
+    index, extension = i.split('.')
     if not extension == cnf.image.extension:
         continue
-    img = Image.open(os.path.join(images_folder,i)) # image extension *.png,*.jpg
+    img = Image.open(os.path.join(images_folder ,i)) # image extension *.png,*.jpg
     img = img.resize((cnf.image.width, cnf.image.height), Image.ANTIALIAS)
     img = img_to_array(img) / 255.
     try:
-        predicted_embedding = model.predict(tf.expand_dims(img,axis=0)).flatten()
+        predicted_embedding = model.predict(tf.expand_dims(img, axis=0)).flatten()
     except ValueError:
         print(i)
         continue
@@ -49,35 +49,40 @@ for i in os.listdir(images_folder):
 
 
 # save annoy index
-t.build(cnf.annoy.ntrees) # number of trees that will be build
+t.build(cnf.annoy.ntrees)  # number of trees that will be build
 t.save('rijk-embedding.ann')
 
 # load annoy index
 u = AnnoyIndex(cnf.model.embeddingsize, 'euclidean')
-u.load('rijk-embedding.ann') # super fast
+u.load('rijk-embedding.ann')  # super fast
 
 
-datadir = 'train'
+testdir = os.path.join(p, 'data/test')  # sample from this directory
 error_counter = 0
 successfully_predict_counter = 0
-samp=4
-with tqdm(total=4323*samp) as pbar:
-    for folder in os.listdir(datadir):
-        sample_dest = os.listdir(os.path.join(datadir,folder))
+samp = 4
+numberofclasses = len(os.listdir(images_folder))
+total = numberofclasses * samp
+with tqdm(total=total) as pbar:
+    for folder in os.listdir(testdir):
+        sample_dest = os.listdir(os.path.join(testdir, folder))  # class to sample
         if len(sample_dest) < samp:
             continue
-        for img in random.sample(sample_dest,samp):
-            image_path = os.path.join(datadir,folder,img)
-            img = Image.open(image_path) # image extension *.png,*.jpg
+        for img in random.sample(sample_dest, samp):
+            image_path = os.path.join(testdir, folder,img)
+            img = Image.open(image_path)
             img = img_to_array(img) / 255.
             try:
-                predicted_embedding = model.predict(tf.expand_dims(img,axis=0)).flatten()
+                predicted_embedding = model.predict(tf.expand_dims(img, axis=0)).flatten()
             except:
-                error_counter+=1
+                error_counter += 1
                 continue
-            result = u.get_nns_by_vector(predicted_embedding,1)  # folder is the
+            result = u.get_nns_by_vector(predicted_embedding, 1)  # folder is the
             if result[0] == int(folder):
-                successfully_predict_counter+=1
+                successfully_predict_counter += 1
             pbar.update(1)
 
-print(successfully_predict_counter / 4323.)
+
+with open(os.path.join(p, "metrics.txt"), 'w') as outfile:
+    precision_at_one = successfully_predict_counter / total
+    outfile.write("precision @1: " + str(precision_at_one) + "\n")
