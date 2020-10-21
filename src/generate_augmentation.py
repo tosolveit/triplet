0c8e2f5a5081fc1b0ae055f7120234f37fc13a6b
macbook <macbook@84.local>
2020-10-21 02:19:51 +0200

    refactored and renamed augmentation

--- src/generate_augmentation.py
+++ src/generate_augmentation.py
@@ -4,25 +4,28 @@ import shutil
 import ruamel.yaml
 from box import Box
 
-# get configuration file path
-p = os.path.abspath(os.path.join(__file__, "../.."))
-params_path = os.path.join(p, 'params.yaml')
 
-# read config based on params, params.yaml is at the project root
-cnf = Box.from_yaml(filename=params_path, Loader=ruamel.yaml.Loader)
+def setup():
+    # get configuration file path
+    p = os.path.abspath(os.path.join(__file__, "../.."))
+    params_path = os.path.join(p, 'params.yaml')
 
-# define train and test data path
-train_stage = os.path.join(p, 'data/train_stage')
-test_stage = os.path.join(p, 'data/test_stage')
+    # read config based on params, params.yaml is at the project root
+    cnf = Box.from_yaml(filename=params_path, Loader=ruamel.yaml.Loader)
 
-# source and destination paths to save processed outputs
-datasets = dict()
-datasets[train_stage] = os.path.join(p, 'data/train')
-datasets[test_stage] = os.path.join(p, 'data/test')
+    # define train and test data path
+    train_stage = os.path.join(p, 'data/train_stage')
+    test_stage = os.path.join(p, 'data/test_stage')
+
+    # source and destination paths to save processed outputs
+    datasets = dict()
+    datasets[train_stage] = os.path.join(p, 'data/train')
+    datasets[test_stage] = os.path.join(p, 'data/test')
+    return cnf, datasets
 
 
-def fileToFolder(datadir, exten='jpeg'):
-    '''
+def file_to_folder(datadir, exten='jpeg'):
+    """
     for a given directory it will put every file in a directory inside this directory and name this
     newly created directory with the file name.
     example:
@@ -40,7 +43,7 @@ def fileToFolder(datadir, exten='jpeg'):
     datadir: directory that you want to transform, string
     exten: jpeg, png like extension of the file,
         other file types will be ignored if they are different than extension, string
-    '''
+    """
     for image in os.listdir(datadir):
         new_folder_name, extension = image.split('.')
         if not extension == exten:
@@ -53,37 +56,31 @@ def fileToFolder(datadir, exten='jpeg'):
     print('successfully created folders inside %s' % datadir)
 
 
-def augmentation_strategy(datadir, width=120, height=120, nsamples=50000):
-    p = Augmentor.Pipeline(datadir)
-    p.resize(probability=1,width=width,height=height,resample_filter=u'NEAREST')
+def augment_images(source_directory, output_directory, width=120, height=120, nsamples=50000):
+    p = Augmentor.Pipeline(source_directory=source_directory,output_directory=output_directory)
+    p.resize(probability=1, width=width, height=height, resample_filter=u'NEAREST')
     p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
     p.zoom(probability=0.5, min_factor=1.1, max_factor=1.3)
-    p.random_brightness(probability=0.3,min_factor=0.4,max_factor=1.4)
-    p.random_color(probability=0.3,min_factor=0.5,max_factor=1.0)
-    p.random_contrast(probability=0.3,min_factor=0.7,max_factor=1.0)
-    #p.random_erasing()
-    #p.random_distortion
-    p.crop_random(probability=0.3,percentage_area=0.7)
-    p.skew_tilt(probability=0.4,magnitude=0.5)
-    p.skew_corner(probability=0.1,magnitude=0.3)
+    p.random_brightness(probability=0.3, min_factor=0.4, max_factor=1.4)
+    p.random_color(probability=0.3, min_factor=0.5, max_factor=1.0)
+    p.random_contrast(probability=0.3, min_factor=0.7, max_factor=1.0)
+    p.crop_random(probability=0.3, percentage_area=0.7)
+    p.skew_tilt(probability=0.4, magnitude=0.5)
+    p.skew_corner(probability=0.1, magnitude=0.3)
     p.sample(nsamples)
 
 
-def generate_augmentation(data):
-# apply strategies and organize folders
-    augmentation_strategy(datadir=data,
-                          width=cnf.image.width,
-                          height=cnf.image.height,
-                          nsamples=cnf.augment.nsamples)
-    outputsavelocationpath = os.path.join(p, 'data/output')
-    #if data == 'train':
-        #movegroundtruth_to_train(datadir=data)
-    shutil.copytree(src=os.path.join(data, 'output'), dst=outputsavelocationpath)
-    shutil.rmtree(path=data)
-    shutil.move(src=outputsavelocationpath, dst=data)
-    
+def main():
+    cnf, datasets = setup()
+    for data_stage, data_dest in datasets.items():
+        file_to_folder(data_stage)
+        augment_images(source_directory=data_stage,
+                       output_directory=data_dest,
+                       width=cnf.image.width,
+                       height=cnf.image.height,
+                       nsamples=cnf.image.nsamples)
 
-for source, destination in datasets.items():
-    shutil.copytree(source, destination)
-    fileToFolder(destination)
-    generate_augmentation(destination)
+
+if __name__ == '__main__':
+    main()
+
