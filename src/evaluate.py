101d94dc3a0b6b535ed9fa9930a125679839901f
macbook <macbook@84.local>
2020-11-28 16:04:23 +0100

    bug fixes

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from annoy import AnnoyIndex
 from PIL import Image
+import tensorflow_addons as tfa
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import array_to_img
@@ -19,8 +20,7 @@ class Evaluate:
                  params_file_name='params.yaml',
                  images_folder_name='data/pix512',
                  images_test_folder_name='data/test',
-                 project_root=(Path(__file__) / ".." / ".."),
-                 custom_objects={'GeMPoolingLayer': GeMPoolingLayer},
+                 custom_objects={'GeMPoolingLayer': GeMPoolingLayer, 'loss': tfa.losses.TripletSemiHardLoss()},
                  sample_size=4
                  ):
         """
@@ -43,19 +43,19 @@ class Evaluate:
         self.params_file_name = params_file_name
         self.images_folder_name = images_folder_name
         self.images_test_folder_name = images_test_folder_name
-        self.project_root = project_root
         self.custom_objects = custom_objects
         self.sample_size = sample_size
 
         # initialize params.yaml file
-        self.params_path = project_root.joinpath(self.params_file_name).resolve()
-        self.images_folder = project_root.joinpath(self.images_folder_name).iterdir()
-        self.images_test_folder = project_root.joinpath(self.images_test_folder_name).iterdir()
+        self.params_path = Path((Path(__file__) / ".." / "..").resolve()).joinpath(self.params_file_name).resolve()
+        self.images_folder = Path((Path(__file__) / ".." / "..").resolve()).joinpath(self.images_folder_name).iterdir()
+        self.images_test_folder = Path((Path(__file__) / ".." / "..").resolve()).joinpath(self.images_test_folder_name).iterdir()
         self.cnf = Box.from_yaml(filename=self.params_path, Loader=ruamel.yaml.Loader)
 
         # initialize index object
         self.t = AnnoyIndex(self.cnf.model.embeddingsize, 'euclidean')
         self.__number_of_classes = len(dir(self.images_test_folder))
+
 
     def load_model(self):
         """
@@ -80,6 +80,8 @@ class Evaluate:
         model = self.load_model()
         for img_path in self.images_folder:
             index, img = self.read_image_and_convert_to_array(img_path=img_path)
+            if img.shape[-1] == 1:
+                img = np.repeat(img[:, :, np.newaxis], 3, axis=2)
             predicted_embedding = model.predict(tf.expand_dims(img, axis=0)).flatten()
             self.t.add_item(int(index), predicted_embedding)
         return self.t
@@ -92,14 +94,14 @@ class Evaluate:
 
     def load_index(self):
         # load annoy index
-        u = AnnoyIndex(self.cnf.model.embeddingsize, self.cnf.annoy.similiarity)
+        u = AnnoyIndex(self.cnf.model.embeddingsize, self.cnf.annoy.similarity)
         u.load(self.cnf.annoy.index_name)  # super fast
         print(f"index loaded as: {self.cnf.annoy.index_name}")
         return u
 
     @staticmethod
     def _get_list_of_the_files_in_a_directory(directory):
-        return list(directory.glob('*'))
+        return list(Path(directory).glob('*'))
 
     def sample_from_directory(self, directory):
         images_in_class = Evaluate._get_list_of_the_files_in_a_directory(directory=directory)
@@ -111,15 +113,17 @@ class Evaluate:
         total = self.__number_of_classes * self.sample_size
         counter = 0
         with tqdm(total=total) as pbar:
-            for test_class in dir(self.images_test_folder):
+            for test_class in self.images_test_folder:
                 # skip if the items inside the class is less than sample size
-                if len(dir(test_class)) < self.sample_size:
+                if len(os.listdir(test_class)) < self.sample_size:
                     continue
                 for sample in self.sample_from_directory(test_class):
                     index, img = self.read_image_and_convert_to_array(img_path=sample)
+                    if img.shape[-1] == 1:
+                        img = np.repeat(img[:, :, np.newaxis], 3, axis=2)
                     predicted_embedding = model.predict(tf.expand_dims(img, axis=0)).flatten()
                     result = index_base.get_nns_by_vector(predicted_embedding, 1)
-                    if result[0] == int(test_class):
+                    if result[0] == int(test_class.name):
                         counter += 1
                     pbar.update(1)
         result = counter / float(total)
@@ -133,6 +137,5 @@ class Evaluate:
 
 if __name__ == '__main__':
     evaluate = Evaluate()
-    evaluate.save_index(index_name=evaluate.cnf.annoy.index.name)
+    evaluate.save_index(index_name=evaluate.cnf.annoy.index_name)
     evaluate.write_metric('metrics_file.txt')
-
